9afeea17252758bf0d369f40fc5654909dd054b1
Muhammed daş <muhammed.das@ithinka.com>
2022-12-19 16:01:58 +0300

    Router&Ngx Gallery added

--- SehirRehberi.SPA/src/app/app.module.ts
+++ SehirRehberi.SPA/src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './route';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
 import { CityComponent } from './city/city.component';
+import { CityDetailComponent } from './city/city-detail/city-detail.component';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [			
+  declarations: [				
     AppComponent,
       ValueComponent,
       NavComponent,
-      CityComponent
+      CityComponent,
+      CityDetailComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule,
+    BrowserAnimationsModule
+    
+   
 
   ],
   providers: [],
--- SehirRehberi.SPA/src/app/services/city.service.ts
+++ SehirRehberi.SPA/src/app/services/city.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { City } from '../models/city';
+import { Photo } from '../models/photo';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,21 @@ export class CityService {
 
 constructor(private httpClient:HttpClient) { }
 path="https://localhost:7031/api/";
+
+
 getCities():Observable<City[]>{
   return this.httpClient.get<City[]>(this.path+"cities");
 }
+
+
+getCityById(cityId:number):Observable<City>{
+  
+return this.httpClient.get<City>(this.path+"cities/detail/?id="+cityId)
+}
+
+getPhotoByCity(cityId:number):Observable<Photo[]>{
+  return this.httpClient.get<Photo[]>(this.path+"cities/photos/?cityId"+cityId)
+
+}
+
 }
